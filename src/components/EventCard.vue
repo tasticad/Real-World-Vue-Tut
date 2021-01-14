af6e0aad664c41964fd813461648eff58eda044f
Adrien Tasic <tasticad@gmail.com>
2021-01-14 14:49:49 +0100

    First push of this Vue app tutorial

---
+++ src/components/EventCard.vue
@@ -0,0 +1,42 @@
+<template>
+  <router-link 
+  class="event-link" 
+  :to="{ name: 'EventDetails', params: { id: event.id } }">
+    <div class="event-card">
+    <span>@ {{ event.time }} on {{ event.date }}</span>
+    <h4>{{ event.title }}</h4>
+    </div>
+  </router-link>
+</template>
+
+<script>
+export default {
+  name: "EventCard",
+  props: {
+    event: Object
+  }
+}
+</script>
+
+<style scoped>
+.event-card {
+  padding: 20px;
+  width: 250px;
+  border: 1px solid #39495c;
+  margin-bottom: 24px;
+  transition: all 0.2s linear;
+  cursor: pointer;
+}
+.event-card:hover {
+  transform: scale(1.01);
+  box-shadow: 0 3px 12px 0 rgba(0, 0, 0, 0.2), 0 1px 15px 0 rgba(0, 0, 0, 0.19);
+}
+.event-card > .title {
+  margin: 0;
+}
+.event-link {
+  color: black;
+  text-decoration: none;
+  font-weight: 100;
+}
+</style>
